9d54f8b4d7a4959704e5bb1571106bd873fd25de
tranphuquy19 <tranphuquy19@gmail.com>
2019-11-16 00:11:53 +0700

    edit controllers models

--- db/roles_seed.js
+++ db/roles_seed.js
@@ -21,55 +21,6 @@ var grantsList = [
     { role: 'guest', resource: 'user', action: 'create:any', attributes: '*, !role' },
     { role: 'guest', resource: 'user', action: 'read:any', attributes: '*, !role, !password, !email, !token, !__v' },
 
-    //  SONG Resource
-    { role: 'admin', resource: 'song', action: 'create:any', attributes: '*' },
-    { role: 'admin', resource: 'song', action: 'read:any', attributes: '*, !__v' },
-    { role: 'admin', resource: 'song', action: 'update:any', attributes: '*' },
-    { role: 'admin', resource: 'song', action: 'delete:any', attributes: '*' },
-
-    { role: 'user', resource: 'song', action: 'create:any', attributes: '*' },
-    { role: 'user', resource: 'song', action: 'read:any', attributes: '*, !__v' },
-    { role: 'user', resource: 'song', action: 'update:own', attributes: '*' },
-    { role: 'user', resource: 'song', action: 'delete:own', attributes: '*' },
-
-    { role: 'guest', resource: 'song', action: 'read:any', attributes: '*, !__v' },
-
-    //  ALBUM Resource
-    { role: 'admin', resource: 'album', action: 'create:any', attributes: '*' },
-    { role: 'admin', resource: 'album', action: 'read:any', attributes: '*, !__v' },
-    { role: 'admin', resource: 'album', action: 'update:any', attributes: '*, !description, !name' },
-    { role: 'admin', resource: 'album', action: 'delete:any', attributes: '*' },
-
-    { role: 'user', resource: 'album', action: 'create:any', attributes: '*' },
-    { role: 'user', resource: 'album', action: 'read:any', attributes: '*, !__v' },
-    { role: 'user', resource: 'album', action: 'update:own', attributes: '*' },
-    { role: 'user', resource: 'album', action: 'delete:own', attributes: '*' },
-
-    { role: 'guest', resource: 'album', action: 'read:any', attributes: '*, !__v' },
-
-    //  CATEGORY Resource
-    { role: 'admin', resource: 'category', action: 'create:any', attributes: '*' },
-    { role: 'admin', resource: 'category', action: 'read:any', attributes: '*, !__v' },
-    { role: 'admin', resource: 'category', action: 'update:any', attributes: '*' },
-    { role: 'admin', resource: 'category', action: 'delete:any', attributes: '*' },
-
-    { role: 'user', resource: 'category', action: 'read:any', attributes: '*, !__v' },
-
-    { role: 'guest', resource: 'category', action: 'read:any', attributes: '*, !__v' },
-
-    //  ARTIST Resource
-    { role: 'admin', resource: 'artist', action: 'create:any', attributes: '*' },
-    { role: 'admin', resource: 'artist', action: 'read:any', attributes: '*, !__v' },
-    { role: 'admin', resource: 'artist', action: 'update:any', attributes: '*' },
-    { role: 'admin', resource: 'artist', action: 'delete:any', attributes: '*' },
-
-    { role: 'user', resource: 'artist', action: 'create:any', attributes: '*' },
-    { role: 'user', resource: 'artist', action: 'read:any', attributes: '*, !__v' },
-    { role: 'user', resource: 'artist', action: 'update:own', attributes: '*' },
-    { role: 'user', resource: 'artist', action: 'delete:own', attributes: '*' },
-
-    { role: 'guest', resource: 'artist', action: 'read:any', attributes: '*, !__v' },
-
     //  COMMENT Resource
     { role: 'admin', resource: 'comment', action: 'create:any', attributes: '*' },
     { role: 'admin', resource: 'comment', action: 'read:any', attributes: '*, !__v' },
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var songController = require('../app/controllers/SongsController');
 var userController = require('../app/controllers/UsersController');
-var albumController = require('../app/controllers/AlbumsController');
 
 var { middlewareJWT } = require('../app/middleware/middlewareJwt');
 var uploader = require('../app/middleware/uploader');
@@ -27,31 +25,5 @@ router.route('/users/:resourceId')
   .patch(middlewareJWT, userController.update)
   .put(middlewareJWT, userController.update)
   .delete(middlewareJWT, userController.delete);
-
-
-/* ROUTE song page */
-router.route('/songs')
-  .get(middlewareJWT, songController.index)
-  .post(middlewareJWT,
-    uploader.fields([{ name: 'imageUpload', maxCount: 1 }, { name: 'songUpload', maxCount: 1 }]),
-    songController.new);
-
-router.route('/songs/:resourceId')
-  .get(middlewareJWT, songController.view)
-  .patch(middlewareJWT,
-    uploader.fields([{ name: 'imageUpload', maxCount: 1 }]),
-    songController.update)
-  .delete(middlewareJWT, songController.delete);
-
-
-/* ROUTE album page */
-router.route('/albums')
-  .get(middlewareJWT, albumController.index)
-  .post(middlewareJWT, uploader.fields([{ name: 'imageUpload', maxCount: 1 }]), albumController.new);
-
-router.route('/albums/:resourceId')
-  .get(middlewareJWT, albumController.view)
-  .patch(middlewareJWT, uploader.fields([{ name: 'imageUpload' }]), albumController.update)
-  .delete(middlewareJWT, albumController.delete);
 
 module.exports = router;
