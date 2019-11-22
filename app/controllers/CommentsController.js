d94b95124cac271d79226249b2440a9117a77f70
tranphuquy19 <tranphuquy19@gmail.com>
2019-11-22 18:45:26 +0700

    push faker

--- app/controllers/CommentsController.js
+++ app/controllers/CommentsController.js
@@ -4,19 +4,33 @@ const Comment = require('../models/CommentModel');
 const User = require('../models/UserModel');
 const Tour = require('../models/TourModel');
 const Review = require('../models/ReviewModel');
+const faker = require('faker');
 
 let resources = {
     'Tour': Tour,
     'Review': Review
 }
 
+let comment = {
+    createdAt: '',
+    creator: {
+        _id: '',
+        name: ''
+    },
+    content: ''
+};
+
 module.exports = {
     index: async (req, res, next) => {
-        // let { resourceId } = req.params;
-        // let tours = await Tour.find().populate('comments');
-        // if (tours) {
-        //     res.state(200).json(tours);
-        // } else next();
+        let comments = [];
+        for (let i = 0; i < 4; i++) {
+            comment.createdAt = new Date().format('m-d-Y h:i:s');
+            comment.creator.name = faker.name.findName();
+            comment.creator._id = faker.helpers.replaceSymbolWithNumber("################");
+            comment.content = faker.hacker.phrase();
+            comments.push(comments);
+        }
+        res.status(200).json(comments);
     },
 
     new: async (req, res, next) => {
